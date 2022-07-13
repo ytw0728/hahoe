10717bd1b7be387809619d4056b3e4e70ed4bdd3
ytw0728 <ytw0728@woowahan.com>
2022-07-13 22:32:37 +0900

    :sparkles: 카메라 전환 추가

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,10 +1,22 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use terrain::model::pixel::Pixel;
 use terrain::*;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
+use wasm_bindgen::{JsCast, JsValue};
+use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader, console, HtmlInputElement};
+use ndarray::{array, Array1};
+use nalgebra::{Matrix4};
+use std::f32;
 
 const ZERO_HEIGHT: f32 = 0.0;
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) -> () {
+    web_sys::window()
+        .unwrap()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
 
 fn set_color(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
     if bitmap.is_empty() {
@@ -14,18 +26,42 @@ fn set_color(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
         return;
     }
 
-    let width = bitmap.len();
-    let height = bitmap[0].len();
+    let width = bitmap.len() - 1;
+    let height = bitmap[0].len() - 1;
     let mut color = Vec::<f32>::with_capacity((width * height * 24) as usize);
 
-    for x in 0..width {
-        for y in 0..height {
-            for _ in 0..6 {
-                color.push(0.0f32);
-                color.push(bitmap[x][y].height as f32);
-                color.push(0.0f32);
-                color.push(1.0f32);
-            }
+    for x in 0..width{
+        for y in 0..height{
+            color.push(0.2f32);
+            color.push(bitmap[x][y].height as f32);
+            color.push(0.2f32);
+            color.push(1.0f32);
+
+            color.push(0.2f32);
+            color.push(bitmap[x+1][y].height as f32);
+            color.push(0.2f32);
+            color.push(1.0f32);
+
+            color.push(0.2f32);
+            color.push(bitmap[x][y+1].height as f32);
+            color.push(0.2f32);
+            color.push(1.0f32);
+
+            color.push(0.2f32);
+            color.push(bitmap[x][y+1].height as f32);
+            color.push(0.2f32);
+            color.push(1.0f32);
+
+            color.push(0.2f32);
+            color.push(bitmap[x+1][y].height as f32);
+            color.push(0.2f32);
+            color.push(1.0f32);
+
+            color.push(0.2f32);
+            color.push(bitmap[x+1][y+1].height as f32);
+            color.push(0.2f32);
+            color.push(1.0f32);
+
         }
     }
 
@@ -40,30 +76,36 @@ fn set_color(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
     }
 }
 
-fn make_triangle_position(x1: f32, x2: f32, y1: f32, y2: f32) -> Vec<f32> {
+fn make_triangle_position(x1: f32, x2: f32, y1: f32, y2: f32, h1: f32, h2: f32, h3: f32, h4: f32) -> Vec<f32> {
     let mut vertices = Vec::<f32>::with_capacity(18);
     vertices.push(x1);
     vertices.push(y1);
-    vertices.push(ZERO_HEIGHT);
+    vertices.push(h1);
+
     vertices.push(x2);
     vertices.push(y1);
-    vertices.push(ZERO_HEIGHT);
+    vertices.push(h2);
+
     vertices.push(x1);
     vertices.push(y2);
-    vertices.push(ZERO_HEIGHT);
+    vertices.push(h3);
+
     vertices.push(x1);
     vertices.push(y2);
-    vertices.push(ZERO_HEIGHT);
+    vertices.push(h3);
+
+    vertices.push(x2);
+    vertices.push(y2);
+    vertices.push(h4);
+
     vertices.push(x2);
     vertices.push(y1);
-    vertices.push(ZERO_HEIGHT);
-    vertices.push(x2);
-    vertices.push(y2);
-    vertices.push(ZERO_HEIGHT);
+    vertices.push(h2);
+
     vertices
 }
 
-fn set_rectangle(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
+fn set_rectangle(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>, program: &WebGlProgram, camera_rad_inputs: &[HtmlInputElement; 4]) {
     if bitmap.is_empty() {
         return;
     }
@@ -71,8 +113,8 @@ fn set_rectangle(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
         return;
     }
 
-    let width = bitmap.len();
-    let height = bitmap[0].len();
+    let width = bitmap.len() - 1;
+    let height = bitmap[0].len() - 1;
 
     let width_ratio = 2. / width as f32;
     let height_ratio = 2. / height as f32;
@@ -82,14 +124,57 @@ fn set_rectangle(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
 
     let mut vertices = Vec::<f32>::with_capacity((width * height * 18) as usize);
 
+    let camera_rads = camera_rad_inputs.clone().map(|node| {
+        let value = node.value();
+        match value.parse::<f32>() {
+            Ok(value) => value,
+            Err(_) => 0.0_32
+        }
+    });
+
+    let x = (camera_rads[0] - 50.0) / 50.0 * 2.0;
+    let y = (camera_rads[1] - 50.0) / 50.0 * 2.0;
+    let z = (camera_rads[2] - 50.0) / 50.0 * 2.0;
+    let d = camera_rads[3];
+
+    let matrix_location = context.get_uniform_location(program, "u_matrix");
+
+    let default = Matrix4::new(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, d,
+    );
+
+    let rotate_x = Matrix4::new(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, x.cos(), -x.sin(), 0.0,
+        0.0, x.sin(), x.cos(), 0.0,
+        0.0, 0.0, 0.0, 1.0,
+    );
+    let rotate_y = Matrix4::new(
+        y.cos(), 0.0, y.sin(), 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        -y.sin(), 0.0, y.cos(), 0.0,
+        0.0, 0.0, 0.0, 1.0,
+    );
+
+    let rotate_z = Matrix4::new(
+        z.cos(), -z.sin(), 0.0, 0.0,
+        z.sin(), z.cos(), 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0,
+    );
+
+    context.uniform_matrix4fv_with_f32_array(matrix_location.as_ref(), false, (&rotate_x * &rotate_y * &rotate_z * &default).as_slice());
+
     for x in 0..width {
         for y in 0..height {
             let x1 = start_width_ratio + x as f32 * width_ratio;
             let x2 = start_width_ratio + (x + 1) as f32 * width_ratio;
             let y1 = start_height_ratio + y as f32 * height_ratio;
             let y2 = start_height_ratio + (y + 1) as f32 * height_ratio;
-
-            vertices.append(&mut make_triangle_position(x1, x2, y1, y2));
+            vertices.append(&mut make_triangle_position(x1, x2, y1, y2, bitmap[x][y].height as f32, bitmap[x+1][y].height as f32, bitmap[x][y+1].height as f32, bitmap[x+1][y+1].height as f32));
         }
     }
 
@@ -111,7 +196,16 @@ fn set_rectangle(context: &WebGl2RenderingContext, bitmap: &Vec<Vec<Pixel>>) {
 pub fn start() -> Result<(), JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
+    let ranges = [
+        HtmlInputElement::from(JsValue::from(document.get_element_by_id("x_range").unwrap())),
+        HtmlInputElement::from(JsValue::from(document.get_element_by_id("y_range").unwrap())),
+        HtmlInputElement::from(JsValue::from(document.get_element_by_id("z_range").unwrap())),
+        HtmlInputElement::from(JsValue::from(document.get_element_by_id("d_range").unwrap())),
+    ];
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    canvas.style().set_property("width", "100vw")?;
+    canvas.style().set_property("height", "100vh")?;
 
     let context = canvas
         .get_context("webgl2")?
@@ -122,16 +216,16 @@ pub fn start() -> Result<(), JsValue> {
         &context,
         WebGl2RenderingContext::VERTEX_SHADER,
         r##"#version 300 es
- 
-        in vec4 position;
+        in vec4 a_position;
         in vec4 a_color;
+
+        uniform mat4 u_matrix;
 
         out vec4 v_color;
 
         void main() {
-        
-            gl_Position = position;
-            v_color = a_color;
+          gl_Position = u_matrix * a_position;
+          v_color = a_color;
         }
         "##,
     )?;
@@ -140,14 +234,13 @@ pub fn start() -> Result<(), JsValue> {
         &context,
         WebGl2RenderingContext::FRAGMENT_SHADER,
         r##"#version 300 es
-    
         precision highp float;
 
         in vec4 v_color;
-        out vec4 outColor;
 
+        out vec4 outColor;
         void main() {
-            outColor = v_color;
+          outColor = v_color;
         }
         "##,
     )?;
@@ -156,7 +249,7 @@ pub fn start() -> Result<(), JsValue> {
     context.use_program(Some(&program));
 
     let color_attribute_location = context.get_attrib_location(&program, "a_color");
-    let position_attribute_location = context.get_attrib_location(&program, "position");
+    let position_attribute_location = context.get_attrib_location(&program, "a_position");
 
     let vao = context
         .create_vertex_array()
@@ -206,8 +299,15 @@ pub fn start() -> Result<(), JsValue> {
     context.clear_color(0.0, 0.0, 0.0, 1.0);
     context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
-    set_rectangle(&context, &bitmap);
+    let f = Rc::new(RefCell::new(None));
+    let g = Rc::clone(&f);
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        set_rectangle(&context, &bitmap, &program, &ranges);
 
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
     Ok(())
 }
 
