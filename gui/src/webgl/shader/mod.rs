e71117d4dadbac2e425b2556db36360dc6f868f4
karl <elrion018@gmail.com>
2022-08-21 14:48:02 +0900

    Refactor : 각 쉐이더 소스 상수 처리

--- gui/src/webgl/shader/mod.rs
+++ gui/src/webgl/shader/mod.rs
@@ -1,5 +1,30 @@
 use web_sys::{WebGl2RenderingContext, WebGlShader};
 
+const VERTEX_SHADER_SOURCE: &str = r##"#version 300 es
+in vec4 a_position;
+in vec4 a_color;
+
+uniform mat4 u_matrix;
+
+out vec4 v_color;
+
+void main() {
+  gl_Position = u_matrix * a_position;
+  v_color = a_color;
+}
+"##;
+
+const FRAGMENT_SHADER_SOURCE: &str = r##"#version 300 es
+precision highp float;
+
+in vec4 v_color;
+
+out vec4 outColor;
+void main() {
+  outColor = v_color;
+}
+"##;
+
 fn compile_shader(
     context: &WebGl2RenderingContext,
     shader_type: u32,
@@ -28,35 +53,14 @@ pub fn get_shaders(context: &WebGl2RenderingContext) -> (WebGlShader, WebGlShade
     let vert_shader = compile_shader(
         &context,
         WebGl2RenderingContext::VERTEX_SHADER,
-        r##"#version 300 es
-        in vec4 a_position;
-        in vec4 a_color;
-
-        uniform mat4 u_matrix;
-
-        out vec4 v_color;
-
-        void main() {
-          gl_Position = u_matrix * a_position;
-          v_color = a_color;
-        }
-        "##,
+        VERTEX_SHADER_SOURCE,
     )
     .unwrap();
 
     let frag_shader = compile_shader(
         &context,
         WebGl2RenderingContext::FRAGMENT_SHADER,
-        r##"#version 300 es
-        precision highp float;
-
-        in vec4 v_color;
-
-        out vec4 outColor;
-        void main() {
-          outColor = v_color;
-        }
-        "##,
+        FRAGMENT_SHADER_SOURCE,
     )
     .unwrap();
 
