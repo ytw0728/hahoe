4ece858f08463eddf29310d5a84650b2d46504c1
sungwooHa <ttlhi10@gmail.com>
2022-08-31 20:14:01 +0900

    clippy & fmt

--- terrain/src/model/topology/particle_pool.rs
+++ terrain/src/model/topology/particle_pool.rs
@@ -1,30 +1,30 @@
+use super::particle::Particle;
 use rust_3d::Point3D;
-use super::particle::{Particle, self};
 
 pub struct ParticlePool {
-    particles : Vec<Particle>,
+    particles: Vec<Particle>,
 }
 
 impl ParticlePool {
     fn new() -> Self {
         ParticlePool {
-            particles : Vec::new(),
+            particles: Vec::new(),
         }
     }
 
-    fn InsertParticle(&mut self, number : u32, position : Point3D){
+    fn insert_particle(&mut self, number: u32, position: Point3D) {
         self.particles.append(
-                &mut (0..number).map(|_| -> Particle { Particle::new_Point3D(&position)})
-                .collect::<Vec<_>>()
-            );
+            &mut (0..number)
+                .map(|_| -> Particle { Particle::new_point3d(&position) })
+                .collect::<Vec<_>>(),
+        );
     }
 
-    fn ReplaceParticle(&mut self, particles : Vec<Particle>) {
+    fn replace_particle(&mut self, particles: Vec<Particle>) {
         self.particles = particles;
     }
 
-    fn GetRefPaticle(&self) -> &Vec<Particle>{
+    fn get_ref_paticle(&self) -> &Vec<Particle> {
         &self.particles
     }
 }
-
