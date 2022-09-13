cba46d44e252d1f7c0e6c20238fdfdec51258d5f
karl <elrion018@gmail.com>
2022-09-13 23:27:48 +0900

    Refactor : 일부 변수명 변경

--- gui/src/webgl/buffer/init.rs
+++ gui/src/webgl/buffer/init.rs
@@ -1,7 +1,10 @@
 use web_sys::{WebGl2RenderingContext, WebGlProgram};
 
-pub fn bind_color_buffer(context: &WebGl2RenderingContext, program:&WebGlProgram) {
-    let color_buffer = context.create_buffer().ok_or("Failed to create buffer").unwrap();
+pub fn bind_color_buffer(context: &WebGl2RenderingContext, program: &WebGlProgram) {
+    let color_buffer = context
+        .create_buffer()
+        .ok_or("Failed to create buffer")
+        .unwrap();
     context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&color_buffer));
     let color_attribute_location = context.get_attrib_location(&program, "a_color");
     context.enable_vertex_attrib_array(color_attribute_location as u32);
@@ -15,8 +18,11 @@ pub fn bind_color_buffer(context: &WebGl2RenderingContext, program:&WebGlProgram
     );
 }
 
-pub fn bind_vertex_buffer(context: &WebGl2RenderingContext, program:&WebGlProgram) {
-    let buffer = context.create_buffer().ok_or("Failed to create buffer").unwrap();
+pub fn bind_vertex_buffer(context: &WebGl2RenderingContext, program: &WebGlProgram) {
+    let buffer = context
+        .create_buffer()
+        .ok_or("Failed to create buffer")
+        .unwrap();
     context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
     let position_attribute_location = context.get_attrib_location(&program, "a_position");
     context.enable_vertex_attrib_array(position_attribute_location as u32);
